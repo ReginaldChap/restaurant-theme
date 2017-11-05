6550173519b271e13c66267a3516e9f4e7aad4cb
Jesse <jesse@jessechapman.com.au>
2017-11-05 19:11:13 +1100

    Fixed food menu bold bumping issue. Added <a> to main menu

--- js/app.js
+++ js/app.js
@@ -21,18 +21,30 @@ document.getElementById("dinnertoggle").addEventListener("click", dinner);
 
 function breakfast() {
     document.getElementById("breakfast-menu").className = "menu-active"; //make class breakfast-active
+    document.getElementById("breaktoggle").className = "active"
     document.getElementById("lunch-menu").className = "lunch-inactive-right"; //make class lunch-inactive-right
+    document.getElementById("lunchtoggle").className = "inactive";
     document.getElementById("dinner-menu").className = "dinner-inactive";
+    document.getElementById("dinnertoggle").className = "inactive";
+    
 }
 function lunch() {
     document.getElementById("lunch-menu").className = "menu-active";
+    document.getElementById("lunchtoggle").className = "active";
     document.getElementById("breakfast-menu").className = "breakfast-inactive";
-    document.getElementById("dinner-menu").className = "dinner-inactive";    
+    document.getElementById("breaktoggle").className = "inactive"    
+    document.getElementById("dinner-menu").className = "dinner-inactive";
+    document.getElementById("dinnertoggle").className = "inactive";
+    
 }
 function dinner() {
     document.getElementById("dinner-menu").className = "menu-active";
+    document.getElementById("dinnertoggle").className = "active";    
     document.getElementById("breakfast-menu").className = "breakfast-inactive";
+    document.getElementById("breaktoggle").className = "inactive"        
     document.getElementById("lunch-menu").className = "lunch-inactive-left";
+    document.getElementById("lunchtoggle").className = "inactive";
+    
     
 }
 /* END Menu Slider */
